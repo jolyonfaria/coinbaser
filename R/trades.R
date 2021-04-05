65c6692aed54b44fc2a5178f23bcb41fab4caa2a
jolyon_faria <jolyon_faria@yahoo.co.uk>
2021-04-05 10:59:29 +0100

    {JSF} Added trades functions.

---
+++ R/trades.R
@@ -0,0 +1,48 @@
+#' Get recent trades
+#'
+#' @description Get recent trades from Coinbase
+#'
+#' @param coin_pair Currency pair: base - quote
+
+#' @return A data.frame of trades
+#' @export
+get_trades <- function(coin_pair = "BTC-USD") {
+  trades <- jsonlite::fromJSON(paste0("https://api.pro.coinbase.com/products/", coin_pair, "/trades"))
+  return(trades)
+}
+
+get_trades_sample <- function(n = 10, coin_pair = "BTC-USD") {
+  btc_trades <- list()
+  for (i in 1:n) {
+    btc_trades[[i]] <- get_trades(coin_pair)
+    Sys.sleep(5)
+    print(i)
+  }
+  btc_trades_all <- dplyr::bind_rows(btc_trades)
+  return(btc_trades_all)
+}
+# 
+# combine_trades <- function(trades) {
+#   btc_trades_all <- bind_rows(trades)
+#   btc_trades_all$time <- lubridate::ymd_hms(btc_trades_all$time)
+#   btc_trades_all$price <- as.numeric(btc_trades_all$price)
+#   btc_trades_all$size <- as.numeric(btc_trades_all$size)
+#   btc_trades_all$value <- btc_trades_all$price * btc_trades_all$size
+#   btc_trades_all <- btc_trades_all[order(btc_trades_all$time), ]
+#   return(btc_trades_all)
+# }
+# 
+# plot_coin <- function(x) {
+#   opar <- par(mfrow = c(1, 3))
+#   hist(x$value, breaks = 50)
+#   plot(
+#     x$time,
+#     x$value * ifelse(x$side == "buy", 1, -1)
+#   )
+#   plot(x$time, x$price, "type" = "l")
+#   par(opar)
+# }
+# 
+# pc_buy <- function(x) {
+#   return(sum(x$side == "buy") * 100 / 1000)
+# }
