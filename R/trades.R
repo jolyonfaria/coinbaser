1c1b9ac2380d64a0b04c63b95d3fda4340561b9e
jolyon_faria <jolyon_faria@yahoo.co.uk>
2021-04-05 11:10:16 +0100

    {JSF} Added trades functions.

--- R/trades.R
+++ R/trades.R
@@ -11,6 +11,15 @@ get_trades <- function(coin_pair = "BTC-USD") {
   return(trades)
 }
 
+#' Get sample of recent trades
+#'
+#' @description Get recent trades from Coinbase
+#'
+#' @param n NUmber of samples
+#' @param coin_pair Currency pair: base - quote
+
+#' @return A data.frame of trades
+#' @export
 get_trades_sample <- function(n = 10, coin_pair = "BTC-USD") {
   btc_trades <- list()
   for (i in 1:n) {
@@ -21,28 +30,50 @@ get_trades_sample <- function(n = 10, coin_pair = "BTC-USD") {
   btc_trades_all <- dplyr::bind_rows(btc_trades)
   return(btc_trades_all)
 }
-# 
-# combine_trades <- function(trades) {
-#   btc_trades_all <- bind_rows(trades)
-#   btc_trades_all$time <- lubridate::ymd_hms(btc_trades_all$time)
-#   btc_trades_all$price <- as.numeric(btc_trades_all$price)
-#   btc_trades_all$size <- as.numeric(btc_trades_all$size)
-#   btc_trades_all$value <- btc_trades_all$price * btc_trades_all$size
-#   btc_trades_all <- btc_trades_all[order(btc_trades_all$time), ]
-#   return(btc_trades_all)
-# }
-# 
-# plot_coin <- function(x) {
-#   opar <- par(mfrow = c(1, 3))
-#   hist(x$value, breaks = 50)
-#   plot(
-#     x$time,
-#     x$value * ifelse(x$side == "buy", 1, -1)
-#   )
-#   plot(x$time, x$price, "type" = "l")
-#   par(opar)
-# }
-# 
-# pc_buy <- function(x) {
-#   return(sum(x$side == "buy") * 100 / 1000)
-# }
+
+#' Combine samples of trades
+#'
+#' @description Combine samples of trades
+#'
+#' @param trades Set of samples of tradea
+
+#' @return A data.frame of trades
+#' @export
+combine_trades <- function(trades) {
+  btc_trades_all <- dplyr::bind_rows(trades)
+  btc_trades_all$time <- lubridate::ymd_hms(btc_trades_all$time)
+  btc_trades_all$price <- as.numeric(btc_trades_all$price)
+  btc_trades_all$size <- as.numeric(btc_trades_all$size)
+  btc_trades_all$value <- btc_trades_all$price * btc_trades_all$size
+  btc_trades_all <- btc_trades_all[order(btc_trades_all$time), ]
+  return(btc_trades_all)
+}
+
+#' Plot trades
+#'
+#' @description Plot set of samples of trades
+#'
+#' @param x Trades
+#' @return None
+#' @export
+plot_coin <- function(x) {
+  opar <- par(mfrow = c(1, 3))
+  hist(x$value, breaks = 50)
+  plot(
+    x$time,
+    x$value * ifelse(x$side == "buy", 1, -1)
+  )
+  plot(x$time, x$price, "type" = "l")
+  par(opar)
+}
+
+#' Percent buyers
+#'
+#' @description Percent buyers
+#'
+#' @param x Trades
+#' @return Percent buyers
+#' @export
+pc_buy <- function(x) {
+  return(sum(x$side == "buy") * 100 / 1000)
+}
